ae024f22bc06ec2af4beb60862ac71a32677bbba
Hiring Circle USA <guttaitl@yahoo.com>
2026-04-16 17:15:54 -0400

    force fix cors

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],   # 🔥 TEMP FIX (important)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
